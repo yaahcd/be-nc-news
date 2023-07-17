d2da0e6f257044c588367aa383ed09bb98fe5717
yaahcd <cremaschi.yara@gmail.com>
2023-07-17 09:46:14 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const apiRouter = require('./routers/api-router');
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api', apiRouter);
