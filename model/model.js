ee4266f4609e0f0e55b37fac574f3d4b38dae4d0
yaahcd <cremaschi.yara@gmail.com>
2023-07-18 15:27:20 +0100

    correction to psql query

--- model/model.js
+++ model/model.js
@@ -91,7 +91,7 @@ exports.selectArticlesById = (id) => {
 			`SELECT a.article_id, a.title, a.topic, a.author, a.body, a.created_at, a.votes, a.article_img_url, 
 			COUNT(c.article_id) AS comment_count 
 			FROM articles a 
-			JOIN comments c ON a.article_id = c.article_id 
+			LEFT JOIN comments c ON a.article_id = c.article_id 
 			WHERE a.article_id = $1
 			GROUP BY a.article_id;`,
 			[id]
