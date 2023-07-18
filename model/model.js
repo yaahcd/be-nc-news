eaef62cd9ddf85eb15dfafc5ef41bd600f3728ad
yaahcd <cremaschi.yara@gmail.com>
2023-07-18 15:15:25 +0100

    correction to sql query

--- model/model.js
+++ model/model.js
@@ -31,7 +31,7 @@ exports.selectAllArticles = (
 		let query = `SELECT a.article_id, a.title, a.topic, a.author, a.created_at, a.votes, a.article_img_url, 
 		COUNT(c.article_id) AS comment_count 
 		FROM articles a 
-		LEFT JOIN comments c ON a.article_id = c.article_id `;
+		LEFT OUTER JOIN comments c ON a.article_id = c.article_id `;
 
 		let queryValues = [];
 		if (topic) {
